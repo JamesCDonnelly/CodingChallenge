4854721471bcfd3c2cb0a2b03f08d83a611da9e3
donzil <firstmanfallen@hotmail.com>
2022-12-01 14:43:53 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Reflection.PortableExecutable;
+
+namespace CodingChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Creature mako = new Creature(Species.Reptilia, Diet.Carnivore, Behaviour.Diurnal);
+
+            Console.WriteLine($"the creature most closely resembles the {mako.species} animal class. It is a {mako.diet} and has has a {mako.behaviour} sleeping pattern.");
+        }
+
+
+
+      
+
+
+        public enum Species { Mammalia, Aves, Reptilia, Amphibia }
+        public enum Diet { Herbivore, Omnivore, Carnivore }
+        public enum Behaviour { Nocturnal, Diurnal, matutinal, vespertine } //night-active, day-active, morning-active, evening-active
+
+        public record Creature(Species species, Diet diet, Behaviour behaviour);
+
+
+    }
+
+}
